8b575de33dfbf675d01364b389e0ee110492872c
cassio <cassiopaixaoti@gmail.com>
2018-01-28 09:57:03 -0300

    ajustes

--- src/configuration/nodeMailer.js
+++ src/configuration/nodeMailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
-const pug = require('pug');
-const juice = require('juice');
-const htmlToText = require('html-to-text');
+// const pug = require('pug');
+// const juice = require('juice');
+// const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
 
 const transport = nodemailer.createTransport({
